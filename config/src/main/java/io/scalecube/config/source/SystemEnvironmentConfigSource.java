5734638b448522229485f20ddb4cf5720be82a57
artem-v <artem.vysochyn@gmail.com>
2017-10-28 22:28:50 +0300

    Added namespace filtering to system env and system props config sources

--- config/src/main/java/io/scalecube/config/source/SystemEnvironmentConfigSource.java
+++ config/src/main/java/io/scalecube/config/source/SystemEnvironmentConfigSource.java
@@ -2,18 +2,40 @@ package io.scalecube.config.source;
 
 import io.scalecube.config.ConfigProperty;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Stream;
 
 public class SystemEnvironmentConfigSource implements ConfigSource {
+  private final List<String> namespaces;
+
+  public SystemEnvironmentConfigSource() {
+    this.namespaces = Collections.emptyList();
+  }
+
+  public SystemEnvironmentConfigSource(List<String> namespaces) {
+    this.namespaces = new ArrayList<>(namespaces);
+  }
+
+  public SystemEnvironmentConfigSource(String... namespaces) {
+    this.namespaces = Arrays.asList(namespaces);
+  }
 
   @Override
   public Map<String, ConfigProperty> loadConfig() {
     Map<String, String> env = System.getenv();
     Map<String, ConfigProperty> result = new TreeMap<>();
+    Stream<String> namespaces1 = namespaces.stream();
+
     for (Map.Entry<String, String> entry : env.entrySet()) {
       String propName = entry.getKey();
-      result.put(propName, LoadedConfigProperty.forNameAndValue(propName, entry.getValue()));
+      if (namespaces.isEmpty() || namespaces1.anyMatch(propName::startsWith)) {
+        result.put(propName, LoadedConfigProperty.forNameAndValue(propName, entry.getValue()));
+      }
     }
     return result;
   }
--- config/src/main/java/io/scalecube/config/source/SystemPropertiesConfigSource.java
+++ config/src/main/java/io/scalecube/config/source/SystemPropertiesConfigSource.java
@@ -2,20 +2,42 @@ package io.scalecube.config.source;
 
 import io.scalecube.config.ConfigProperty;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
+import java.util.stream.Stream;
 
 public class SystemPropertiesConfigSource implements ConfigSource {
+  private final List<String> namespaces;
+
+  public SystemPropertiesConfigSource() {
+    this.namespaces = Collections.emptyList();
+  }
+
+  public SystemPropertiesConfigSource(List<String> namespaces) {
+    this.namespaces = new ArrayList<>(namespaces);
+  }
+
+  public SystemPropertiesConfigSource(String... namespaces) {
+    this.namespaces = Arrays.asList(namespaces);
+  }
 
   @Override
   public Map<String, ConfigProperty> loadConfig() {
     Properties properties = System.getProperties();
     Map<String, ConfigProperty> result = new TreeMap<>();
+    Stream<String> namespaces1 = namespaces.stream();
+
     for (Enumeration<?> e = properties.propertyNames(); e.hasMoreElements();) {
       String propName = (String) e.nextElement();
-      result.put(propName, LoadedConfigProperty.forNameAndValue(propName, properties.getProperty(propName)));
+      if (namespaces.isEmpty() || namespaces1.anyMatch(propName::startsWith)) {
+        result.put(propName, LoadedConfigProperty.forNameAndValue(propName, properties.getProperty(propName)));
+      }
     }
     return result;
   }

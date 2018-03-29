1a93e2e790edb72cfa7232d1c07b6d0bd3ce3c11
aharonha <aharon@marketspulse.com>
2018-03-29 10:34:11 +0300

    tests are now more readable

--- config-vault/src/test/java/io/scalecube/config/vault/TestVaultConfigSource.java
+++ config-vault/src/test/java/io/scalecube/config/vault/TestVaultConfigSource.java
@@ -18,33 +18,46 @@ import java.util.Map;
 
 public class TestVaultConfigSource {
 
+  private static final int VAULT_PORT = 8200;
   private static final String VAULT_TOKEN = "my-root-token";
-  private static final String VAULT_SECRETS_PATH = "secret/application/tenant";
+  /**
+   * the environment variable name for vault secret path
+   */
+  private static final String VAULT_SECRETS_PATH = "VAULT_SECRETS_PATH";
+
+  // these 3 are actual values we would like to test with
+  private static final String VAULT_SECRETS_PATH1 = "secret/application/tenant1";
+  private static final String VAULT_SECRETS_PATH2 = "secret/application/tenant2";
+  private static final String VAULT_SECRETS_PATH3 = "secret/application/tenant3";
+
   @ClassRule
   public static VaultContainer<?> vaultContainer = new VaultContainer<>()
       .withVaultToken(VAULT_TOKEN)
-      .withVaultPort(8200)
-      .withSecretInVault(VAULT_SECRETS_PATH, "top_secret=password1", "db_password=dbpassword1")
-      .withSecretInVault(VAULT_SECRETS_PATH + "2", "top_secret=password2", "db_password=dbpassword2");
+      .withVaultPort(VAULT_PORT)
+      .withSecretInVault(VAULT_SECRETS_PATH1, "top_secret=password1", "db_password=dbpassword1")
+      .withSecretInVault(VAULT_SECRETS_PATH2, "top_secret=password2", "db_password=dbpassword2");
 
-  EnvironmentLoader loader, loader2, loader3;
+  EnvironmentLoader loader1, loader2, loader3;
 
 
   @Before
   public void setUp() throws Exception {
-    Map<String, String> overrides = new HashMap<>();
-    overrides.put("VAULT_TOKEN", VAULT_TOKEN);
-    overrides.put("VAULT_ADDR", "http://" + vaultContainer.getContainerIpAddress() + ":8200");
-    overrides.put("VAULT_SECRETS_PATH", VAULT_SECRETS_PATH);
-    this.loader = new MockEnvironmentLoader(overrides);
+    Map<String, String> commonEnvironmentVariables = new HashMap<>();
+    commonEnvironmentVariables.put("VAULT_TOKEN", VAULT_TOKEN);
+    commonEnvironmentVariables.put("VAULT_ADDR", new StringBuilder("http://")
+        .append(vaultContainer.getContainerIpAddress()).append(':').append(VAULT_PORT).toString());
+    
+    Map<String, String> tenant1 = new HashMap<>(commonEnvironmentVariables);
+    tenant1.put(VAULT_SECRETS_PATH, VAULT_SECRETS_PATH1);
+    this.loader1 = new MockEnvironmentLoader(tenant1);
 
-    Map<String, String> overrides2 = new HashMap<>(overrides);
-    overrides2.put("VAULT_SECRETS_PATH", VAULT_SECRETS_PATH + "2");
-    this.loader2 = new MockEnvironmentLoader(overrides2);
+    Map<String, String> tenant2 = new HashMap<>(commonEnvironmentVariables);
+    tenant2.put(VAULT_SECRETS_PATH, VAULT_SECRETS_PATH2);
+    this.loader2 = new MockEnvironmentLoader(tenant2);
 
-    Map<String, String> overrides3 = new HashMap<>(overrides2);
-    overrides3.put("VAULT_SECRETS_PATH", VAULT_SECRETS_PATH + "3");
-    this.loader3 = new MockEnvironmentLoader(overrides3);
+    Map<String, String> tenant3 = new HashMap<>(commonEnvironmentVariables);
+    tenant3.put(VAULT_SECRETS_PATH, VAULT_SECRETS_PATH3);
+    this.loader3 = new MockEnvironmentLoader(tenant3);
 
 
   }
@@ -71,7 +84,7 @@ public class TestVaultConfigSource {
 
   @Test
   public void test() {
-    VaultConfigSource vaultConfigSource = new VaultConfigSource(loader);
+    VaultConfigSource vaultConfigSource = new VaultConfigSource(loader1);
     Map<String, ConfigProperty> loadConfig = vaultConfigSource.loadConfig();
     ConfigProperty actual = loadConfig.get("top_secret");
     assertThat(actual, notNullValue());

f6af58cfc5b031fbf50e5d7b6c1f96f14fa9ff9c
io-scalecube-ci <io.scalecube.ci@gmail.com>
2018-04-23 06:30:27 +0000

    + script files

--- src/main/scripts/cd/before-deploy.sh
+++ src/main/scripts/cd/before-deploy.sh
@@ -41,9 +41,8 @@ function setupgit {
     git remote set-url origin git@github.com:$TRAVIS_REPO_SLUG.git
 	git config --global user.email "io.scalecube.ci@gmail.com"
     git config --global user.name "io-scalecube-ci"
-    git config --global user.signingkey $GPG_KEY
-	git checkout -B $TRAVIS_BRANCH
-	git reset origin $TRAVIS_BRANCH
+    git config --global user.signingkey $GPG_PASSPHRASE
+	git checkout -B $TRAVIS_BRANCH | true
 }
 
 function deployment {

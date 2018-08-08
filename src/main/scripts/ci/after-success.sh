07465911ddd1d5e0d9d0becccad717737e88cc58
io-scalecube-ci <io.scalecube.ci@gmail.com>
2018-08-08 14:26:11 +0000

    + script files

--- src/main/scripts/ci/after-success.sh
+++ src/main/scripts/ci/after-success.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+echo       Running $0
+echo *-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 if [ -z "$CODACY_PROJECT_TOKEN" ]; then
 	echo [WARNING] Please go to https://app.codacy.com/app/$TRAVIS_REPO_SLUG/settings/coverage and add CODACY_PROJECT_TOKEN to travis settings
 else 

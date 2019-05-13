2d2cd8d84ae5773c317480f6fad98ace6485874e
aharonha <aharon@om2.com>
2019-05-13 14:37:13 +0300

    fix JSON model of tested class

--- config-scalecube-configuration-service/src/test/java/io/scalecube/config/service/ApiKey.java
+++ config-scalecube-configuration-service/src/test/java/io/scalecube/config/service/ApiKey.java
@@ -1,22 +1,98 @@
 package io.scalecube.config.service;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import java.util.Arrays;
 
 public class ApiKey {
-  public String APIKey;
-  public List<String> Permissions;
+
+  private String apiKey;
+  private String[] permissions;
+
+  @JsonCreator
+  public ApiKey(
+      @JsonProperty("APIKey") String apiKey, @JsonProperty("Permissions") String[] permissions) {
+    this.apiKey = apiKey;
+    this.permissions = permissions;
+  }
+
+  @JsonCreator
+  public ApiKey() {
+    this(null, null);
+  }
+
+  /** @return the aPIKey */
+  @JsonGetter("APIKey")
+  public String getAPIKey() {
+    return this.apiKey;
+  }
+
+  /**
+   * @param apiKey the aPIKey to set
+   * @return
+   */
+  @JsonSetter("APIKey")
+  public void setAPIKey(String apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  /** @return the permissions */
+  @JsonGetter("Permissions")
+  public String[] getPermissions() {
+    return this.permissions;
+  }
+
+  /**
+   * @param permissions the permissions to set
+   * @return
+   */
+  @JsonSetter("Permissions")
+  public void setPermissions(String[] permissions) {
+    this.permissions = permissions;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((this.apiKey == null) ? 0 : this.apiKey.hashCode());
+    result = prime * result + Arrays.hashCode(this.permissions);
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    ApiKey other = (ApiKey) obj;
+    if (this.apiKey == null) {
+      if (other.apiKey != null) return false;
+    } else if (!this.apiKey.equals(other.apiKey)) return false;
+    if (!Arrays.equals(this.permissions, other.permissions)) return false;
+    return true;
+  }
+
   /* (non-Javadoc)
    * @see java.lang.Object#toString()
    */
   @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
-    builder
-        .append("ApiKey [APIKey=")
-        .append(this.APIKey)
-        .append(", Permissions=")
-        .append(this.Permissions)
-        .append("]");
+    builder.append("ApiKey [apiKey=");
+    builder.append(this.apiKey);
+    builder.append(", permissions=");
+    builder.append(Arrays.toString(this.permissions));
+    builder.append("]");
     return builder.toString();
   }
+  
 }
--- config-scalecube-configuration-service/src/test/java/io/scalecube/config/service/BrokerData.java
+++ config-scalecube-configuration-service/src/test/java/io/scalecube/config/service/BrokerData.java
@@ -1,31 +1,64 @@
 package io.scalecube.config.service;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import java.util.Arrays;
 
 public class BrokerData {
-  public String BrokerID;
-  public List<ApiKey> APIKeys;
-  
+
+  private String brokerID;
+  private ApiKey[] apiKeys;
+
+  @JsonCreator
+  public BrokerData(
+      @JsonProperty("BrokerID") String brokerID, @JsonProperty("APIKeys") ApiKey[] apiKeys) {
+    this.brokerID = brokerID;
+    this.apiKeys = apiKeys;
+  }
+
+  @JsonCreator
+  public BrokerData() {}
+
+  @JsonSetter("BrokerID")
+  public void setBrokerID(@JsonProperty("BrokerID") String brokerID) {
+    this.brokerID = brokerID;
+  }
+
+  /** @return the brokerID */
+  @JsonGetter("BrokerID")
+  public String getBrokerID() {
+    return this.brokerID;
+  }
+
+  /** @return the aPIKeys */
+  @JsonGetter("APIKeys")
+  public ApiKey[] getApikeys() {
+    return this.apiKeys;
+  }
+
+  /**
+   * @param apiKeys the aPIKeys to set
+   * @return
+   */
+  @JsonSetter("APIKeys")
+  public void setApikeys(@JsonProperty("APIKeys") ApiKey[] apiKeys) {
+    this.apiKeys = apiKeys;
+  }
+
   /* (non-Javadoc)
    * @see java.lang.Object#toString()
    */
-  public BrokerData() {
-    // TODO Auto-generated constructor stub
-  }
-
-  public void setBrokerID(String BrokerID) {
-    this.BrokerID = BrokerID;
-  }
-
   @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
-    builder
-        .append("BrokerData [BrokerID=")
-        .append(this.BrokerID)
-        .append(", APIKeys=")
-        .append(this.APIKeys)
-        .append("]");
+    builder.append("BrokerData [brokerID=");
+    builder.append(this.brokerID);
+    builder.append(", apiKeys=");
+    builder.append(Arrays.toString(this.apiKeys));
+    builder.append("]");
     return builder.toString();
   }
+  
 }
